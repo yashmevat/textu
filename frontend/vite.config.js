c0ab81999679e66f0b44d9ba17c5b1629d133d8c
yash <yashmevat16@gmail.com>
2025-05-17 14:46:41 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
 
+import ghPages from 'vite-plugin-gh-pages';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tsconfigPaths()],
+  plugins: [react(),tsconfigPaths(),ghPages()],
   base:"/textu"
 })
